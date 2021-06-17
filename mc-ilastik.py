4a37b12ee2462ac331fe30756492c62402bd9b1c
Artem Sokolov <artem.sokolov@gmail.com>
2021-06-16 22:02:39 -0400

    Wrapper for interfacing with MCMICRO (#11)

---
+++ mc-ilastik.py
@@ -0,0 +1,22 @@
+import argparse
+import os
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', nargs='*', help="Images for CommandIlastikPrepOME.py")
+    parser.add_argument('--model', help="Custom model for run_ilastik.sh",
+                        default="/app/classifiers/exemplar_001_nuclei.ilp")
+
+    args, pargs = parser.parse_known_args()
+
+    # Construct a CommandIlastikPrepOME.py call
+    cmd1 = "python /app/CommandIlastikPrepOME.py --input " + \
+        " ".join(args.input) + " " + " ".join(pargs)
+    print(cmd1)
+    os.system(cmd1)
+
+    # Construct a run_ilastik.sh call
+    cmd2 = "/ilastik-release/run_ilastik.sh --headless --project " + \
+        str(args.model) + " *.hdf5"
+    print(cmd2)
+    os.system(cmd2)
