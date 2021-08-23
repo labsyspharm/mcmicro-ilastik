46d37c1d82b93b9900d8d4a81683513dadaaa871
Artem Sokolov <artem.sokolov@gmail.com>
2021-08-23 18:37:39 -0400

    Migrated CLI to 1-based indexing of channels

--- ParseInput.py
+++ ParseInput.py
@@ -21,11 +21,22 @@ def ParseInputOME():
    parser.add_argument('--crop_amount', type=int)
 
    args = parser.parse_args()
+
+   #Adjustment to account for user-facing 1-based indexing and the 0-based Python implementation
+   if args.nuclei_index != None:
+      nuc_idx = args.nuclei_index-1
+   else:
+      nuc_idx = None
+   if args.channelIDs != None:
+      chIDs = [x-1 for x in args.channelIDs]
+   else:
+      chIDs = None
+      
    #Create a dictionary object to pass to the next function
    dict = {'input': args.input, 'output': args.output, 'crop': args.crop,\
       'crop_size':args.crop_size,'nonzero_fraction':args.nonzero_fraction,\
-         'nuclei_index':args.nuclei_index, 'num_channels':args.num_channels,\
-         'channelIDs':args.channelIDs,'ring_mask':args.ring_mask, 'crop_amount':args.crop_amount}
+         'nuclei_index':nuc_idx, 'num_channels':args.num_channels,\
+         'channelIDs':chIDs,'ring_mask':args.ring_mask, 'crop_amount':args.crop_amount}
    #Print the dictionary object
    print(dict)
    #Return the dictionary
